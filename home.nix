cc3c5032f2cc67aa4eec11643f5351781c5a514d
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-03-19 00:50:15 -0700

    git and wakatime

--- home.nix
+++ home.nix
@@ -1,13 +1,37 @@
 { lib, pkgs, ... }:
 {
+  nixpkgs.config.allowUnfree = true;
+
   home = {
     packages = with pkgs; [
-      hello
+      hyprland
+      wget
+      git
+      neovim
+      fastfetch
+      ghostty
+      kitty
+      wl-clipboard
+      google-chrome
+      wakatime-cli
     ];
 
     username = "wyatt";
     homeDirectory = "/home/wyatt";
 
     stateVersion = "24.11";
+  };
+
+  programs.git = {
+    userName = "Wyatt Avilla";
+    userEmail = "wyattmurphy1@gmail.com";
+    extraConfig.init.defaultBranch = "main";
+  };
+
+  programs.neovim = {
+    vimAlias = true;
+    plugins = [
+      pkgs.vimPlugins.vim-wakatime
+    ];
   };
 }
