3930b962e172c6f892b26e3abc98d61e84c9e85f
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-03-19 14:23:33 -0700

    hyprland movement and monitor orientation

--- home.nix
+++ home.nix
@@ -4,11 +4,11 @@
 
   home = {
     packages = with pkgs; [
-      hyprland
+      #hyprland
       wget
       fastfetch
       ghostty
-      kitty
+      ghostty
       wl-clipboard
       google-chrome
       wakatime-cli
@@ -44,5 +44,47 @@
       vim.keymap.set({ "n", "v" }, "k", "n", { noremap = true })
       vim.keymap.set({ "n", "v" }, "l", "e", { noremap = true })
     '';
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    package = pkgs.hyprland;
+    settings = {
+      general = {
+        "$modifier" = "SUPER";
+	layout = "master";
+      };
+      monitor = [
+        "DP-1,2560x1440@165,0x0,1"
+        "HDMI-A-1,1920x1080,2560x-200,1,transform,1"
+      ];
+      master = {
+        new_on_top = true;
+      };
+      input = {
+        repeat_rate = 60;
+        repeat_delay = 400;
+      };
+      bind = [
+        "$modifier,code:47,exec,ghostty"
+        "$modifier,w,exec,google-chrome-stable"
+        "$modifier,q,killactive"
+
+        "$modifier,e,layoutmsg,cyclenext"
+        "$modifier,i,layoutmsg,cycleprev"
+        "$modifier,o,resizeactive,10 0"
+        "$modifier,n,resizeactive,-10 0"
+
+	"$modifier,a,workspace,1"
+	"$modifier,r,workspace,2"
+	"$modifier,s,workspace,3"
+	"$modifier,t,workspace,4"
+
+	"$modifier,a,movetoworkspace,1"
+	"$modifier,r,movetoworkspace,2"
+	"$modifier,s,movetoworkspace,3"
+	"$modifier,t,movetoworkspace,4"
+      ];
+    };
   };
 }
