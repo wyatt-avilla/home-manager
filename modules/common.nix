2500aa2081397d4b7ba1b02253f34aafb4c2a0a9
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-03-20 20:59:18 -0700

    zathura

--- modules/common.nix
+++ modules/common.nix
@@ -61,6 +61,23 @@
     settings.confirm-close-surface = false;
   };
 
+  programs.zathura = {
+    enable = true;
+    mappings = {
+      "n" = "scroll left";
+      "e" = "scroll down";
+      "i" = "scroll up";
+      "o" = "scroll right";
+
+      "E" = "navigate next";
+      "I" = "navigate previous";
+    };
+
+    options = {
+      "selection-clipboard" = "clipboard";
+    };
+  };
+
   programs.neovim = {
     enable = true;
     vimAlias = true;
