682b6f28e4de4d902d605f70bc3d2cad5df40be6
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-03-25 18:46:22 -0700

    determine workspace at startup

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -44,6 +44,7 @@ let
     	esac
     }
 
+    determine_occupancy
     ${socat} -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
   '';
 
