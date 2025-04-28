d18a0f5a24d08be93a6f3cd3eed0b39f2278d758
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-04-27 18:15:47 -0700

    use wayland option for spicetify instead of overlay

--- modules/spicetify.nix
+++ modules/spicetify.nix
@@ -5,23 +5,11 @@ in
 {
   programs.spicetify = {
     enable = true;
+    wayland = true;
 
     enabledExtensions = with spicePkgs.extensions; [
       keyboardShortcut
       shuffle
     ];
   };
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      spotify = prev.spotify.overrideAttrs (oldAttrs: {
-        postInstall =
-          (oldAttrs.postInstall or "")
-          + ''
-            substituteInPlace $out/share/applications/spotify.desktop \
-              --replace "spotify %U" "spotify %U --enable-features=UseOzonePlatform --ozone-platform=wayland"
-          '';
-      });
-    })
-  ];
 }
