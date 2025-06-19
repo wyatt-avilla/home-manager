fd2b93436405e16830f78ea8d56675d546bbcd88
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-06-19 13:57:07 -0700

    fix writeText function

--- hosts/laptop/keyboard.nix
+++ hosts/laptop/keyboard.nix
@@ -1,10 +1,11 @@
+{ pkgs, ... }:
 {
   # relevant keyboard FRU: 45N2242 45N2102 45N2172
 
   services.xserver.xkb.extraLayouts.colemak-dh-swaps = {
     description = "Colemak-DH Utilizing Japanese Keys";
     languages = [ "eng" ];
-    symbolsFile = builtins.writeFile {
+    symbolsFile = pkgs.writeTextFile {
       name = "colemak-dh-swaps";
       text = ''
         xkb_symbols "colemak-dh-swaps" {
