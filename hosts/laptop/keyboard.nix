e745022ac5fdc91848dc8fafe72c72aa3ca77cfb
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-06-19 14:08:26 -0700

    use xdg xkb file for keymap

--- hosts/laptop/keyboard.nix
+++ hosts/laptop/keyboard.nix
@@ -1,20 +1,17 @@
-{ pkgs, ... }:
+let
+  xkbFileName = "colemak-dh-swaps";
+in
 {
   # relevant keyboard FRU: 45N2242 45N2102 45N2172
 
-  services.xserver.xkb.extraLayouts.colemak-dh-swaps = {
-    description = "Colemak-DH Utilizing Japanese Keys";
-    languages = [ "eng" ];
-    symbolsFile = pkgs.writeTextFile {
-      name = "colemak-dh-swaps";
-      text = ''
-        xkb_symbols "colemak-dh-swaps" {
-            include "us(colemak_dh_ortho)"
+  xdg.configFile."xkb/symbols/us/${xkbFileName}" = {
+    text = ''
+      xkb_symbols "${xkbFileName}" {
+        include "us(colemak_dh_ortho)"
 
-            key <MUHE> { [ BackSpace, BackSpace ] };
-            key <HENK> { [ space,     space ] };
-        };
-      '';
-    };
+        key <MUHE> { [ BackSpace, BackSpace ] };
+        key <HENK> { [ space,     space ] };
+      };
+    '';
   };
 }
