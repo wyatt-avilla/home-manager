e0a287a1ccc753ec302b576ff746159bbbfeffd6
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-07-30 18:47:30 -0700

    fix asset paths

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Home Manager Configuration
 
-[![wakatime](https://wakatime.com/badge/user/d98bbc46-d96b-4611-b0b4-3956f04fcaa0/project/a54825ed-8235-4cef-a5a6-010ba8cf39f4.svg)](https://wakatime.com/badge/user/d98bbc46-d96b-4611-b0b4-3956f04fcaa0/project/a54825ed-8235-4cef-a5a6-010ba8cf39f4)
-
-## Screenshot
-
-![Desktop Screenshot](https://raw.githubusercontent.com/wiki/wyatt-avilla/home-manager/screenshots/desktop_screenshot.png)
+![Desktop Screenshot](./assets/screenshots/desktop_screenshot_simple.png)
--- modules/hyprland/hyprpaper.nix
+++ modules/hyprland/hyprpaper.nix
@@ -5,44 +5,9 @@
   ...
 }:
 let
-  wallpapers = [
-    (pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/wiki/wyatt-avilla/home-manager/wallpapers/dark_monstera.png";
-      sha256 = "sha256-qoxl1gaLyHQXdkcpuVOumsfGneCEZc45WHv2JGEgoBc=";
-    })
-    (pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/wiki/wyatt-avilla/home-manager/wallpapers/grainy_ferns.png";
-      sha256 = "sha256-UEFiiEfOEcDfzZ2aK4n9zOfmjJpIwyVFcYNoWB2D4UQ=";
-    })
-    (pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/wiki/wyatt-avilla/home-manager/wallpapers/mononoke_mountains.png";
-      sha256 = "sha256-CLv/2yxiEvb3/Edx7GJT59izs0/MaoWYF/PHXoqific=";
-    })
-    (pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/wiki/wyatt-avilla/home-manager/wallpapers/mononoke_rocks.png";
-      sha256 = "sha256-c7OZi5XI6iQqOpdxqfkKlcGi+eIxtDrdSqel/fstwkY=";
-    })
-    (pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/wiki/wyatt-avilla/home-manager/wallpapers/small_purple_flower.png";
-      sha256 = "sha256-ree5/DRZRB9ixiwPyZeebc544x8kXdh7qGMRLZI5oFA=";
-    })
-    (pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/wiki/wyatt-avilla/home-manager/wallpapers/variety_leaves.png";
-      sha256 = "sha256-gNsEG4U0tO+OkKOy5n+qWZtmlzPRn6DUUsivDeMur1Q=";
-    })
-    (pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/wiki/wyatt-avilla/home-manager/wallpapers/mononoke_cliff.png";
-      sha256 = "sha256-evdyx6ks0qbiwH9o0uG7LYUlvwV3M+iVHEz2v+Ggao8=";
-    })
-    (pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/wiki/wyatt-avilla/home-manager/wallpapers/spirited_away_forest.png";
-      sha256 = "sha256-Pxh6cwuGpgxLvy3ZBTl6gO694q7wcUCRXaMs4QOJyO8=";
-    })
-    (pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/wiki/wyatt-avilla/home-manager/wallpapers/spirited_away_tunnel.png";
-      sha256 = "sha256-psbTKbpIzCvNmUAQMiJKuc4lYu+Ip1Ogyw8QpUUJ0Ts=";
-    })
-  ];
+  wallpapers = builtins.filter (path: pkgs.lib.filesystem.pathIsRegularFile path) (
+    pkgs.lib.filesystem.listFilesRecursive ../../assets/wallpapers
+  );
 
   randomWallpaperScript = pkgs.writeShellScript "set-random-wallpaper" ''
     #!/usr/bin/env bash
