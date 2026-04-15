5664a505d97143146be2ae7abb8edeefb0bb27ec
Wyatt Avilla <wyatt@linux.com>
2026-04-15 00:09:37 -0700

    update flake

--- modules/opencode.nix
+++ modules/opencode.nix
@@ -2,9 +2,9 @@
 {
   programs.opencode = {
     enable = true;
+    tui.theme = lib.mkForce "system";
     settings = {
       plugin = [ "@mohak34/opencode-notifier@0.1.19" ];
-      theme = lib.mkForce "system";
     };
   };
 }
