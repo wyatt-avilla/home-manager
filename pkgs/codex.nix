47cfeb4a2fb8e0e50380e6b13fde5917b278b953
Wyatt Avilla <wyatt@linux.com>
2026-09-05 09:20:00 -0700

    update codex

--- pkgs/codex.nix
+++ pkgs/codex.nix
@@ -1,23 +1,44 @@
 {
-  lib,
+  autoPatchelfHook,
   fetchurl,
+  glibc,
+  lib,
+  ncurses,
   stdenvNoCC,
 }:
 
 stdenvNoCC.mkDerivation rec {
   pname = "codex";
-  version = "0.144.6";
+  version = "0.153.4";
 
   src = fetchurl {
-    url = "https://github.com/openai/codex/releases/download/rust-v${version}/codex-x86_64-unknown-linux-musl.tar.gz";
-    hash = "sha256-ap3vUaCtjOpmhNjrO/AzyJ8z47xc/kkvGh4KcYRRocY=";
+    url = "https://github.com/openai/codex/releases/download/rust-v${version}/codex-package-x86_64-unknown-linux-musl.tar.gz";
+    hash = "sha256-qCIYfhokIMYcWSZyG/vYeHAe2VVHybsNTeRJiha6GCE=";
+  };
+
+  appServerSrc = fetchurl {
+    url = "https://github.com/openai/codex/releases/download/rust-v${version}/codex-app-server-x86_64-unknown-linux-musl.tar.gz";
+    hash = "sha256-rODnlMU9DBq+L9uSSGhJBNBLCKylp4UbxKfOCId3PPA=";
   };
 
   sourceRoot = ".";
 
+  nativeBuildInputs = [ autoPatchelfHook ];
+
+  buildInputs = [
+    glibc
+    ncurses
+  ];
+
   installPhase = ''
     runHook preInstall
-    install -Dm755 codex-x86_64-unknown-linux-musl "$out/bin/codex"
+
+    mkdir -p "$out"
+    cp -r bin codex-package.json codex-path codex-resources "$out/"
+
+    tar -xzf "$appServerSrc"
+    install -Dm755 codex-app-server-x86_64-unknown-linux-musl "$out/bin/codex-app-server"
+
     runHook postInstall
   '';
 
